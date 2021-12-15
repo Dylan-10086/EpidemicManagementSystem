c07f4bc2bbdf73d605f96fca858682de293366c8
zitao <liuzitao0123@foxmail.com>
2021-12-15 13:39:01 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/EpidemicManagementSystem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EpidemicManagementSystem.dir/main.c.o"
+  "EpidemicManagementSystem"
+  "EpidemicManagementSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EpidemicManagementSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
