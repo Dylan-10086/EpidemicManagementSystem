679f8f44644aa8dccd8c87f5981bb17f07264c72
zitao <liuzitao0123@foxmail.com>
2021-12-28 00:26:28 +0800

    完成了人员信息结构体的编写，及链表的适配

--- LinkList.c
+++ LinkList.c
@@ -1,6 +1,3 @@
-//
-// Created by 小学二年级的刘同学 on 2021/10/8.
-//
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
--- LinkList.h
+++ LinkList.h
@@ -1,7 +1,3 @@
-//
-// Created by 小学二年级的刘同学 on 2021/10/8.
-//
-
 #ifndef LINKLIST_LINKLIST_H
 
 #include <stdlib.h>
